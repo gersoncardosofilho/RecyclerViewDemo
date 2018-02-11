06e6a5ee7a78a680f3d1c51ec7b2f1afb46cf349
Gerson Cardoso Filho <gersoncfilho@mac.com>
2018-02-11 10:33:34 -0200

    Initial commit

---
+++ app/src/main/java/com/example/gersondeveloper/recyclerviewdemo/DetailActtivity.java
@@ -0,0 +1,10 @@
+package com.example.gersondeveloper.recyclerviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gerso on 2/11/2018.
+ */
+
+public class DetailActtivity extends AppCompatActivity {
+}
